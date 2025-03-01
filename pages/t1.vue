7c771ee9de404a5f31a9226587c10aae527c8f81
lorenzo <lollo.diaspro@gmail.com>
2025-03-01 10:37:17 +0100

    wip: new simple graphic

---
+++ pages/t1.vue
@@ -0,0 +1,15 @@
+<!-- NEW INDEX PAGE -->
+
+<template>
+  <div class="d-flex flex-column justify-content-center align-items-center vh-100">
+    <h1 class="text-center mb-3">BoardGames Hub</h1>
+    <a class="btn btn-primary" href="/log1">Get Started</a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+/* Stili personalizzati opzionali */
+</style>
